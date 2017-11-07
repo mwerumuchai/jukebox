eec5defcec9653ece8d53283f2535304a6e4b110
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-11-07 18:04:24 +0300

    Custom 403 page Create custom 403 error page when an authorised user tries to access another groups page

--- app/main/errors.py
+++ app/main/errors.py
@@ -9,3 +9,12 @@ def four_Ow_four(error):
     '''
     title = '404 page'
     return render_template('fourOwfour.html', title=title),404
+
+# Error handler decorator
+@main.app_errorhandler(403)
+def four_Ow_three(error):
+    '''
+    Function to render the 403 error page
+    '''
+    title = '403 page'
+    return render_template('fourOwthree.html', title=title),403
