fe4ff0e164b1ecd2a47896c3e45e8e035f882c02
mwerumuchai <mwerumuchai@gmail.com>
2017-11-08 12:31:06 +0300

    Add background color and styling for sidebar

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ class TestConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://muchai:muchai90@localhost/nick_box_test'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://muchai:muchai90@localhost/nick_box_test'
     # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://carol:carol1@localhost/nick_box_test'
 
 
@@ -35,7 +35,7 @@ class DevConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://muchai:muchai90@localhost/nick_box'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://muchai:muchai90@localhost/nick_box'
     # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://carol:carol1@localhost/nick_box'
 
 
