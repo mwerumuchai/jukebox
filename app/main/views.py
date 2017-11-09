ddfaa8acf2944672b218fc4af13ab31d940c0a3b
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-11-09 11:55:02 +0300

    Create search song view function Update index route to return the search song route only when the user is using the search bar Create search route view function that uses search_songs to get songs begining with the user inpu Create displayFoundSong macro to display names of songs found Update search.html to use displayFoundSong macro to display names of songs found

--- app/main/views.py
+++ app/main/views.py
@@ -14,7 +14,22 @@ def index():
     playlists = Playlist.get_playlists() 
     title = 'Home'
 
-    return render_template('index.html', title = title, playlists = playlists)
+    search_song = request.args.get('song_query')
+
+    if search_song:
+        return redirect(url_for('.search_song', song_name = search_song))
+    else: 
+        return render_template('index.html', title = title, playlists = playlists)
+
+@main.route('/search/<song_name>')
+def search_song(song_name):
+    '''
+    View function  for searching a song in the playlist
+    '''
+    found_songs = Song.search_songs(song_name)
+    title = f'{song_name} results'
+    return render_template('search.html', title = title, found_songs = found_songs)
+
 
 @main.route('/playlist/<int:id>')
 def playlist(id):
